5777f269698d7454e885be7f610f0a850315ff74
Dhanushmiller <dhajayaram@gmail.com>
2026-01-30 10:07:34 +0530

    coded

--- reversenum.js
+++ reversenum.js
@@ -1,1 +1,12 @@
 
+function reversenum(n){
+    let reversed=0;
+
+    while(n!=0){
+        let ld=n%10;
+        reversed=reversed*10+ld;
+        n=Math.floor(n/10)
+    }
+    return reversed;
+}
+console.log(reversenum(1234));
